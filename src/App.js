e832065ebe40650dee1320f505c7acb585052214
shamim-behshad <shamim.behshad@gmail.com>
2024-06-09 11:44:22 +0330

    product card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar'
+import Products from './Components/Prodcust';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Products />
+    </>
   );
 }
 
---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProductCard = ({ product, addToCart }) => {
+  return (
+    <div className="border border-gray-300 rounded-lg p-4 relative">
+      <div className="flex flex-col h-full">
+        <img
+          src={product.image}
+          alt="#"
+          className="mx-auto mb-4 rounded-lg max-h-80 object-cover"
+        />
+        <div className="flex-1">
+          <h6>{product.category}</h6>
+          <h6 className="text-lg font-semibold">{product.title}</h6>
+          <p className="text-gray-700 mt-2">{`Description: ${product.description}`}</p>
+        </div>
+        <div className="mt-auto flex justify-between">
+          <h6 className="font-bold text-black py-3">{`Price: ${product.price}`}</h6>
+          <button
+            onClick={() => addToCart(product)}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
