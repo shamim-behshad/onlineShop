311c9b35e84eba9be2309eba037c5c79f3cedb7b
shamim-behshad <shamim.behshad@gmail.com>
2024-06-12 13:49:17 +0330

    making the card a little bit smaller

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const ProductCard = ({ product, addToCart }) => {
   return (
-    <div className="border border-gray-300 rounded-lg p-4 relative">
+    <div className="border border-gray-300 rounded-lg p-3 relative">
       <div className="flex flex-col h-full">
         <img
           src={product.image}
           alt="#"
-          className="mx-auto mb-4 rounded-lg max-h-80 object-cover"
+          className="mx-auto mb-3 rounded-lg max-h-60 object-cover"
         />
         <div className="flex-1">
           <h6>{product.category}</h6>
           <h6 className="text-lg font-semibold">{product.title}</h6>
-          <p className="text-gray-700 mt-2">{`Description: ${product.description}`}</p>
+          <p className="text-gray-700 mt-1">{`Description: ${product.description}`}</p>
         </div>
         <div className="mt-auto flex justify-between">
-          <h6 className="font-bold text-black py-3">{`Price: ${product.price}`}$</h6>
+          <h6 className="font-bold text-black py-2">{`Price: ${product.price}$`}</h6>
           <button
             onClick={() => addToCart(product)}
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+            className="bg-blue-500 text-white px-3 py-1 rounded-lg mt-2"
           >
             Add to Cart
           </button>
